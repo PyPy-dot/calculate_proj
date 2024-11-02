0a222a990fd387c6a8aac4db6a6a15b4a98fe423
PyPy-dot <nerosed131@gmail.com>
2024-11-02 18:25:09 +0500

    0.2

--- main.py
+++ main.py
@@ -1,77 +1,86 @@
 import tkinter as tk
 
 
-def is_valid_data():
-    return entry1.get().isdigit() and entry2.get().isdigit()
+class Calculate:
+    def __new__(cls):
+        _instance = None
+        if _instance is None:
+            _instance = super().__new__(cls)
+        return _instance
 
+    def __init__(self) -> None:
+        self.root = tk.Tk()
+        self.label1 = tk.Label(self.root, text='Введите первое число')
+        self.entry1 = tk.Entry(self.root, width=20)
+        self.label2 = tk.Label(self.root, text='Введите второе число')
+        self.entry2 = tk.Entry(self.root, width=20)
+        self.button_mul = tk.Button(self.root, text='*', width=2, height=1, command=self._mul_func)
+        self.button_sub = tk.Button(self.root, text='-', width=2, height=1, command=self._mul_sub)
+        self.button_add = tk.Button(self.root, text='+', width=2, height=1, command=self._mul_add)
+        self.button_div = tk.Button(self.root, text='/', width=2, height=1, command=self._mul_div)
+        self.label3 = tk.Label(self.root, text='Ответ:')
+        self.entry3 = tk.Entry(self.root, width=40)
 
-def mul_func() -> None:
-    entry3.delete(0, tk.END)
-    if is_valid_data():
-        num1 = int(entry1.get())
-        num2 = int(entry2.get())
-        entry3.insert(0, str(num1 * num2))
-    else:
-        entry3.insert(0, 'Некорректные данные!')
+    def _window(self) -> None:
+        self.root.geometry('350x350')
+        self.root.resizable(False, False)
+        self.label1.place(x=30, y=50)
+        self.entry1.place(x=30, y=80)
+        self.label2.place(x=200, y=50)
+        self.entry2.place(x=200, y=80)
+        self.button_mul.place(x=60, y=150)
+        self.button_sub.place(x=130, y=150)
+        self.button_add.place(x=200, y=150)
+        self.button_div.place(x=270, y=150)
+        self.label3.place(x=30, y=250)
+        self.entry3.place(x=80, y=250)
 
+    def is_valid_data(self) -> bool:
+        return self.entry1.get().isdigit() and self.entry2.get().isdigit()
 
-def mul_sub() -> None:
-    entry3.delete(0, tk.END)
-    if is_valid_data():
-        num1 = int(entry1.get())
-        num2 = int(entry2.get())
-        entry3.insert(0, str(num1 - num2))
-    else:
-        entry3.insert(0, 'Некорректные данные!')
-
-
-def mul_add() -> None:
-    entry3.delete(0, tk.END)
-    if is_valid_data():
-        num1 = int(entry1.get())
-        num2 = int(entry2.get())
-        entry3.insert(0, str(num1 + num2))
-    else:
-        entry3.insert(0, 'Некорректные данные!')
-
-
-def mul_div() -> None:
-    entry3.delete(0, tk.END)
-    if is_valid_data():
-        num1 = int(entry1.get())
-        num2 = int(entry2.get())
-        if num2:
-            entry3.insert(0, str(num1 / num2))
+    def _mul_func(self) -> None:
+        self.entry3.delete(0, tk.END)
+        if self.is_valid_data():
+            num1 = int(self.entry1.get())
+            num2 = int(self.entry2.get())
+            self.entry3.insert(0, str(num1 * num2))
         else:
-            entry3.insert(0, 'На ноль делить нельзя!')
-    else:
-        entry3.insert(0, 'Некорректные данные!')
+            self.entry3.insert(0, 'Некорректные данные!')
+
+    def _mul_sub(self) -> None:
+        self.entry3.delete(0, tk.END)
+        if self.is_valid_data():
+            num1 = int(self.entry1.get())
+            num2 = int(self.entry2.get())
+            self.entry3.insert(0, str(num1 - num2))
+        else:
+            self.entry3.insert(0, 'Некорректные данные!')
+
+    def _mul_add(self) -> None:
+        self.entry3.delete(0, tk.END)
+        if self.is_valid_data():
+            num1 = int(self.entry1.get())
+            num2 = int(self.entry2.get())
+            self.entry3.insert(0, str(num1 + num2))
+        else:
+            self.entry3.insert(0, 'Некорректные данные!')
+
+    def _mul_div(self) -> None:
+        self.entry3.delete(0, tk.END)
+        if self.is_valid_data():
+            num1 = int(self.entry1.get())
+            num2 = int(self.entry2.get())
+            if num2:
+                self.entry3.insert(0, str(num1 / num2))
+            else:
+                self.entry3.insert(0, 'На ноль делить нельзя!')
+        else:
+            self.entry3.insert(0, 'Некорректные данные!')
+
+    def start(self) -> None:
+        self._window()
+        self.root.mainloop()
 
 
-root = tk.Tk()
-root.geometry('350x350')
-root.resizable(False, False)
-
-label1 = tk.Label(root, text='Введите первое число')
-entry1 = tk.Entry(root, width=20)
-label2 = tk.Label(root, text='Введите второе число')
-entry2 = tk.Entry(root, width=20)
-button_mul = tk.Button(root, text='*', width=2, height=1, command=mul_func)
-button_sub = tk.Button(root, text='-', width=2, height=1, command=mul_sub)
-button_add = tk.Button(root, text='+', width=2, height=1, command=mul_add)
-button_div = tk.Button(root, text='/', width=2, height=1, command=mul_div)
-label3 = tk.Label(root, text='Ответ:')
-entry3 = tk.Entry(root, width=40)
-
-label1.place(x=30, y=50)
-entry1.place(x=30, y=80)
-label2.place(x=200, y=50)
-entry2.place(x=200, y=80)
-button_mul.place(x=60, y=150)
-button_sub.place(x=130, y=150)
-button_add.place(x=200, y=150)
-button_div.place(x=270, y=150)
-label3.place(x=30, y=250)
-entry3.place(x=80, y=250)
-
-root.mainloop()
+if __name__ == '__main__':
+    Calculate().start()
